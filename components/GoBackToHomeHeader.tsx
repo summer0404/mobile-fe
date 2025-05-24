dddcbcd51db8cb4544a259f1d227e984fd6acf31
summer0404 <157554348+summer0404@users.noreply.github.com>
2025-05-25 00:46:10 +0700

    Analysis page done

---
+++ components/GoBackToHomeHeader.tsx
@@ -0,0 +1,28 @@
+import { View, Text, TouchableOpacity } from 'react-native'
+import React from 'react'
+import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons'
+import { useRouter } from 'expo-router';
+
+
+
+interface GoBackToHomeHeaderProps {
+    title: string;
+}
+
+const GoBackToHomeHeader = ({ title }: GoBackToHomeHeaderProps) => {
+    const router = useRouter();
+
+    return (
+        <View className="flex-row justify-between items-center mb-6">
+            <TouchableOpacity onPress={() => router.navigate('/home')}>
+                <Ionicons name="arrow-back" size={28} color="white" />
+            </TouchableOpacity>
+            <Text className="text-xl font-pbold text-white">{title}</Text>
+            <TouchableOpacity onPress={() => console.log('Notifications Pressed')}>
+                <MaterialCommunityIcons name="bell-outline" size={28} color="white" />
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+export default GoBackToHomeHeader
